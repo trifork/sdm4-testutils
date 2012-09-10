6ee380408f4da246de619f81da509febd25af153
Jakob Færch <jrf@trifork.com>
2012-09-11 00:11:31 +0200

    StatusPageChecker tillader at klienter kan få adgang til responseBody. Skulle bruges i vitaminimporter

--- src/main/java/dk/nsi/sdm4/testutils/StatuspageChecker.java
+++ src/main/java/dk/nsi/sdm4/testutils/StatuspageChecker.java
@@ -1,5 +1,7 @@
 package dk.nsi.sdm4.testutils;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -14,28 +16,54 @@ import static org.junit.Assert.fail;
  */
 public class StatuspageChecker {
 	private static final int MAX_RETRIES = 10;
+	private String url;
 
-	public void assertThatStatuspageReturns200OK(String modulename) throws IOException, InterruptedException {
-		int status = 0;
-		String url = "http://localhost:8080/" + modulename + "/status";
+	public StatuspageChecker(String modulename) {
+		this.url = "http://localhost:8080/" + modulename + "/status";
+	}
+
+	public void assertThatStatuspageReturns200OK() throws IOException, InterruptedException {
+		StatuspageResult result = fetchStatusPage();
+
+		if (result.status != 200) {
+			fail("Status page on " + url + " did not respond with HTTP code 200 after " + MAX_RETRIES + " retries, last status was " + result.status);
+		}
+	}
+
+	public StatuspageResult fetchStatusPage() throws IOException, InterruptedException {
 		final URL u = new URL(url);
+		StatuspageResult lastResultSeen = null;
 		for (int i = 0; i < MAX_RETRIES; i++) {
 			HttpURLConnection connection = (HttpURLConnection) u.openConnection();
 			connection.connect();
-			status = connection.getResponseCode();
-			connection.disconnect();
 
-			if (status == 200) {
-				return;
+			try {
+				lastResultSeen = new StatuspageChecker.StatuspageResult(connection.getResponseCode(), IOUtils.toString(connection.getInputStream()));
+			} finally {
+				connection.disconnect();
 			}
 
-			if (status != 404 && status != 503) {
-				fail("Status page did not respond with HTTP code 200, status was " + status);
+			if (lastResultSeen.status == 200) {
+				break;
+			}
+
+			if (lastResultSeen.status != 404 && lastResultSeen.status != 503) {
+				fail("Status page on " + url + " did not respond with HTTP code 200, status was " + lastResultSeen.status);
 			}
 
 			Thread.sleep(1000);
 		}
 
-		fail("Status page on " + url + " did not respond with HTTP code 200 after " + MAX_RETRIES + " retries, last status was " + status);
+		return lastResultSeen;
+	}
+
+	public class StatuspageResult {
+		public int status;
+		public String responseBody;
+
+		private StatuspageResult(int status, String responseBody) {
+			this.status = status;
+			this.responseBody = responseBody;
+		}
 	}
 }
